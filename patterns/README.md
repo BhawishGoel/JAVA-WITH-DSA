b2ae49f31b4789b2347de665038a873a71f08003
BhawishGoel <bhawishgoel97@gmail.com>
2026-04-07 20:12:18 +0530

    adding readme file in patterns

---
+++ patterns/README.md
@@ -0,0 +1,7 @@
+# PHP Pattern Programs
+
+## Pattern 1
+File: patterns/pattern_1.php
+
+Output:
+* * * * *
---
+++ patterns/pattern_2.java
@@ -0,0 +1,7 @@
+package patterns;
+
+public class pattern_2 {
+    public static void main(String[] args) {
+        
+    }
+}
