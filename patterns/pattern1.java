f15cc867010f2873308f43ae695726e73ee04e7f
BhawishGoel <bhawishgoel97@gmail.com>
2026-04-07 18:16:34 +0530

    pattern_1

---
+++ patterns/pattern1.java
@@ -0,0 +1,11 @@
+package patterns;
+public class pattern1 {
+    public static void main(String[] args){
+        int star = 5;
+        int i=1;
+        while(i <= star){
+            System.out.print("1 ");
+            i++;
+        }
+    }
+}
