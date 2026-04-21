6a5846670ef4c871a9e66c52e9ae1fb082edffa4
BhawishGoel <bhawishgoel97@gmail.com>
2026-04-21 20:50:04 +0530

    update in pattern -29

--- patterns/pattern29.java
+++ patterns/pattern29.java
@@ -9,6 +9,8 @@ public class pattern29 {
         int row = 1;
         int space = n - 1;
         int star = 1;
+        int val = 1;
+        // both vertical and horizontal mirroring is there 
 
         while(row <= 2 * n - 1 ){
             // space prep 
@@ -20,16 +22,26 @@ public class pattern29 {
 
             // star prep 
             int j = 1;
+            int p = val;
             while(j <= star){
-                System.out.print("* ");
+                System.out.print(p + " ");
+                // vertical mirroring 
+                if(j <= star/2 ){
+                    p++;
+                }else{
+                    p--;
+                }
                 j++;
             }
+            // horizontal mirror concept
             if(row < n){
                 star += 2;
                 space --;
+                val++;
             }else{
                 star -= 2;
                 space++;
+                val--;
             }
             row++;
             System.out.println();
